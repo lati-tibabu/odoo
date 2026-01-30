bd9c6d38eb3af3a073a9b24d0f9983ca2f0e3af2
latitibabu <latitibabu2018@gmail.com>
2026-01-30 16:39:50 +0300

    feat: Change authentication method for Keycloak login route to public

--- custom_addons/auth_keycloak_login_redirect/controllers/main.py
+++ custom_addons/auth_keycloak_login_redirect/controllers/main.py
@@ -6,7 +6,7 @@ import json
 import werkzeug.urls
 
 class KeycloakHome(Home):
-    @http.route('/web/login', type='http', auth="none")
+    @http.route('/web/login', type='http', auth="public")
     def web_login(self, redirect=None, **kw):
         ensure_db()
         
